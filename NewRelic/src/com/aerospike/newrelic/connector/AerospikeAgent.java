64d7ed61f1e75505f5afa345fa24421eaf586c9d
Sud <sudhanshu@aerospike.com>
2016-09-15 02:35:59 -0700

    Fix version num.

--- NewRelic/src/com/aerospike/newrelic/connector/AerospikeAgent.java
+++ NewRelic/src/com/aerospike/newrelic/connector/AerospikeAgent.java
@@ -38,7 +38,7 @@ import com.newrelic.metrics.publish.util.Logger;
 public class AerospikeAgent extends Agent {
     
     private static final String GUID = "com.aerospike.newrelic.connector";
-    private static final String VERSION = "1.0.0";
+    private static final String VERSION = "2.0.0";
     
     private String user;
     private String password;
